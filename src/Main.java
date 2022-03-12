bbbb4c5083632dc44338b4ac206ab05eb475a008
Stanislav Rakitov <prosperousrf@gmail.com>
2022-03-12 19:23:00 +0300

    gitignore

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.io.File;
+
+/**
+ * @author Stanislav Rakitov in 2022
+ */
+public class Main {
+  public static void main(String[] args) {
+
+    // Создадим объекты File для файлов и каталогов:
+    // создаем объект File в качестве каталогаFile
+    // dir1 = new File("C://SomeDir");
+    // создаем объект File для файла, находящегося в каталогеFile file1 = new File("C://SomeDir", "Hello.txt");// создаем объект File для файла, находящегося в каталоге dir1File file2 = new File(dir1, "Hello2.
+  }
+}
