62c5e5fbb81b84c611a962f7f99fbc76bcfd3e60
Stanislav Rakitov <prosperousrf@gmail.com>
2022-03-12 21:21:29 +0300

    main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  * @author Stanislav Rakitov in 2022
@@ -6,9 +9,57 @@ import java.io.File;
 public class Main {
   public static void main(String[] args) {
 
-    // Создадим объекты File для файлов и каталогов:
-    // создаем объект File в качестве каталогаFile
-    // dir1 = new File("C://SomeDir");
-    // создаем объект File для файла, находящегося в каталогеFile file1 = new File("C://SomeDir", "Hello.txt");// создаем объект File для файла, находящегося в каталоге dir1File file2 = new File(dir1, "Hello2.
+    StringBuilder logFile = new StringBuilder();
+    // В папке Games создайте несколько директорий: src, res, savegames, temp.
+    createDirWithLogMessahe(logFile, "H:\\Games\\src");
+    createDirWithLogMessahe(logFile, "H:\\Games\\res");
+    createDirWithLogMessahe(logFile, "H:\\Games\\savegames");
+    createDirWithLogMessahe(logFile, "H:\\Games\\temp");
+
+    // В каталоге src создайте две директории: main, test.
+    createDirWithLogMessahe(logFile, "H:\\Games\\src\\main");
+    createDirWithLogMessahe(logFile, "H:\\Games\\src\\test");
+
+    // В подкаталоге main создайте два файла: Main.java, Utils.java.
+    createFile("H:\\Games\\src\\main\\Main.java", logFile);
+    createFile("H:\\Games\\src\\main\\Utils.java", logFile);
+
+    // В каталог res создайте три директории: drawables, vectors, icons.
+    createDirWithLogMessahe(logFile, "H:\\Games\\res\\drawables");
+    createDirWithLogMessahe(logFile, "H:\\Games\\res\\vectors");
+    createDirWithLogMessahe(logFile, "H:\\Games\\res\\icons");
+
+    // В директории temp создайте файл temp.txt.
+    createFile("H:\\Games\\temp\\temp.txt", logFile);
+
+    // Файл temp.txt должен содержать информацию о том, что все файлы были созданы успешно.
+    try (BufferedWriter bufferedWriter =
+        new BufferedWriter(new FileWriter("H:\\Games\\temp\\temp.txt"))) {
+      bufferedWriter.write(logFile.toString());
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private static void createDirWithLogMessahe(StringBuilder logFile, String dirName) {
+    File srcDir = new File(dirName);
+    logFile
+        .append("Directory ")
+        .append(dirName)
+        .append(srcDir.mkdir() ? " is" : " is not")
+        .append(" " + "created\n");
+  }
+
+  private static void createFile(String fileName, StringBuilder logFile) {
+    File newFile = new File(fileName);
+    try {
+      logFile
+              .append("File ")
+              .append(fileName)
+              .append(newFile.createNewFile() ? " is" : " is not")
+              .append(" created\n");
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 }
