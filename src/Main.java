e556530bc106d384107640d3d3af8f586c47d577
Stanislav Rakitov <prosperousrf@gmail.com>
2022-03-17 14:04:56 +0300

    File.separator

--- src/Main.java
+++ src/Main.java
@@ -9,32 +9,45 @@ import java.io.IOException;
 public class Main {
   public static void main(String[] args) {
 
+    String ROOT_PREFIX = "H:";
+    String GAME_DIR_PREFIX = ROOT_PREFIX + File.separator + "Games" + File.separator;
+
     StringBuilder logFile = new StringBuilder();
     // В папке Games создайте несколько директорий: src, res, savegames, temp.
-    createDirWithLogMessahe(logFile, "H:\\Games\\src");
-    createDirWithLogMessahe(logFile, "H:\\Games\\res");
-    createDirWithLogMessahe(logFile, "H:\\Games\\savegames");
-    createDirWithLogMessahe(logFile, "H:\\Games\\temp");
+    String resDir = GAME_DIR_PREFIX + "res";
+    String srcDir = GAME_DIR_PREFIX + "src";
+    String savegamesDir = GAME_DIR_PREFIX + "savegames";
+    String tempDir = GAME_DIR_PREFIX + "temp";
+    createDirWithLogMessahe(logFile, srcDir);
+    createDirWithLogMessahe(logFile, resDir);
+    createDirWithLogMessahe(logFile, savegamesDir);
+    createDirWithLogMessahe(logFile, tempDir);
 
     // В каталоге src создайте две директории: main, test.
-    createDirWithLogMessahe(logFile, "H:\\Games\\src\\main");
-    createDirWithLogMessahe(logFile, "H:\\Games\\src\\test");
+    String mainDir = srcDir + File.separator + "main";
+    String testDir = srcDir + File.separator + "test";
+    createDirWithLogMessahe(logFile, mainDir);
+    createDirWithLogMessahe(logFile, testDir);
 
     // В подкаталоге main создайте два файла: Main.java, Utils.java.
-    createFile("H:\\Games\\src\\main\\Main.java", logFile);
-    createFile("H:\\Games\\src\\main\\Utils.java", logFile);
+    String mainFile = mainDir + File.separator + "Main.java";
+    String utilsFile = mainDir + File.separator + "Utils.java";
+
+    createFile(mainFile, logFile);
+    createFile(utilsFile, logFile);
 
     // В каталог res создайте три директории: drawables, vectors, icons.
-    createDirWithLogMessahe(logFile, "H:\\Games\\res\\drawables");
-    createDirWithLogMessahe(logFile, "H:\\Games\\res\\vectors");
-    createDirWithLogMessahe(logFile, "H:\\Games\\res\\icons");
+    createDirWithLogMessahe(logFile, resDir + File.separator + "drawables");
+    createDirWithLogMessahe(logFile, resDir + File.separator + "vectors");
+    createDirWithLogMessahe(logFile, resDir + File.separator + "icons");
 
     // В директории temp создайте файл temp.txt.
-    createFile("H:\\Games\\temp\\temp.txt", logFile);
+    String tempFile = tempDir + File.separator + "temp.txt";
+    createFile(tempFile, logFile);
 
     // Файл temp.txt должен содержать информацию о том, что все файлы были созданы успешно.
     try (BufferedWriter bufferedWriter =
-        new BufferedWriter(new FileWriter("H:\\Games\\temp\\temp.txt"))) {
+        new BufferedWriter(new FileWriter(tempFile))) {
       bufferedWriter.write(logFile.toString());
     } catch (IOException e) {
       e.printStackTrace();
